9c6d90dfa68122c6a3d17cce727fa732bf1b6841
Can Aydın <51122560+byc4N@users.noreply.github.com>
2019-05-28 16:28:35 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Check-hostfix
-check-host engelleme v2019
+# check-host engelleme | 2019 |
+# by c4N
+# Basit bir python ile check-host (ping) engellemeyi kapatma hızlı yolu
+# python ile basit kodlama
+# serverbutik.com
+
+# Sadece Linux İşletim Sistemlerinde Geçerlidir! 
+# Kullanımı: python checkhost.py | yazarak çalıştırabilirsiniz.
---
+++ checkhost.py
@@ -0,0 +1,37 @@
+""" 
+@ Created : c4N - www.serverbutik.com
+@ https://github.com/byc4N
+@ E-mail: c4N@serverbutik.com
+@
+"""
+
+import os
+os.system('iptables -A INPUT -s 88.119.179.10/24 -j DROP')
+os.system('iptables -A INPUT -s 5.149.253.121/24 -j DROP')
+os.system('iptables -A INPUT -s 149.202.25.65/24 -j DROP')
+os.system('iptables -A INPUT -s 213.202.222.130/24 -j DROP')
+os.system('iptables -A INPUT -s 46.4.143.48/24 -j DROP')
+os.system('iptables -A INPUT -s 185.25.204.60/24 -j DROP')
+os.system('iptables -A INPUT -s 195.123.211.84/24 -j DROP')
+os.system('iptables -A INPUT -s 178.17.171.235/24 -j DROP')
+os.system('iptables -A INPUT -s 31.148.219.169/24 -j DROP')
+os.system('iptables -A INPUT -s 176.221.34.58/24 -j DROP')
+os.system('iptables -A INPUT -s 185.159.82.88/24 -j DROP')
+os.system('iptables -A INPUT -s 109.248.203.193/24 -j DROP')
+os.system('iptables -A INPUT -s 185.29.8.135/24 -j DROP')
+os.system('iptables -A INPUT -s 179.43.148.195/24 -j DROP')
+os.system('iptables -A INPUT -s 5.22.157.52/24 -j DROP')
+os.system('iptables -A INPUT -s 95.46.98.26/24 -j DROP')
+os.system('iptables -A INPUT -s 185.81.113.195/24 -j DROP')
+os.system('iptables -A INPUT -s 199.38.245.218/24 -j DROP')
+os.system('iptables -A INPUT -s 146.196.65.20/24 -j DROP')
+os.system('iptables -A INPUT -s 31.148.219.0/24 -j DROP')
+os.system('iptables -A INPUT -s 193.187.152.0/24 -j DROP')
+os.system('iptables -A INPUT -s 185.120.77.0/24 -j DROP')
+os.system('iptables -A INPUT -s 93.170.128.0/24 -j DROP')
+os.system('iptables -A INPUT -s 95.46.99.0/24 -j DROP')
+os.system('iptables -A INPUT -s 192.157.233.0/24 -j DROP')
+os.system('iptables -A INPUT -s 198.56.183.0/24 -j DROP')
+os.system('iptables -A INPUT -s 172.246.126.0/24 -j DROP')
+os.system('iptables -A INPUT -s 185.222.202.190 -j DROP')
+os.system('iptables -A INPUT -s 178.239.162.13 -j DROP')
